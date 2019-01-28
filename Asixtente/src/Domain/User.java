92f19a6d3af80148f1ddb89d9b7e51ea8e81aa00
Dimplypaladin <especk47@gmail.com>
2019-01-27 17:44:33 -0700

    updating in user class

--- Asixtente/src/Domain/User.java
+++ Asixtente/src/Domain/User.java
@@ -10,5 +10,28 @@ package Domain;
  * @author Danny Logax
  */
 public class User {
+    String user;
+    String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "user=" + user + ", password=" + password + '}';
+    }
     
 }
